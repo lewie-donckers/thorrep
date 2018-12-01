ea836c85d1a0e990e9da1cc137121ac59ae63d58
Lewie Donckers <l.donckers@xs4all.nl>
2018-12-01 15:38:33 +0100

    Switched to classes.

--- ThorRep.lua
+++ ThorRep.lua
@@ -2,9 +2,6 @@
 local frame = 1
 --
 
-local factionVars = {}
-
-
 -- .Reputation--DEFAULT .Reputation-standing{color:#0f9601}
 -- .Reputation--EXALTED .Reputation-standing{color:#28a687}
 -- .Reputation--REVERED .Reputation-standing{color:#0f9601}
@@ -25,9 +22,6 @@ local factionVars = {}
 -- .Reputation--PERSONAL_WINGMAN .Reputation-standing{color:#0f9601}
 
 -- TODO constants in ALL_CAPS
-
-local init = 0
-local factions = 0
 
 local TR_CHATFRAME = _G["ChatFrame" .. frame]
 local TR_DEBUG = true
@@ -71,107 +65,146 @@ local function LogDebug(message, ...)
     end
 end
 
-local function ScanFactions()
+
+
+
+-- Faction class
+Faction = {}
+Faction.__index = Faction
+
+function Faction:Create(factionID)
+    local object = {}
+    setmetatable(object, Faction)
+    object.factionID = factionID
+
+    local name, _, standingID, _, _, value = GetFactionInfoByID(factionID)
+    object.name = name
+    object.standingID = standingID
+    object.value = value
+
+    -- local friendID, friendRep, friendMaxRep, friendName, friendText, friendTexture, friendTextLevel, friendThreshold, nextFriendThreshold = GetFriendshipReputation(factionID)
+    -- if friendID then
+    --     LogDebug("%s seems to be a friendship", name)
+    -- end
+
+    return object
+end
+
+function Faction:Update()
+    local _, _, standingID, barMin, barMax, barValue = GetFactionInfoByID(self.factionID)
+
+    local diff = barValue - self.value
+    if diff ~= 0 then
+        if standingID ~= self.standingID then
+            local newfaction = _G["FACTION_STANDING_LABEL" .. standingID]
+            local newstandingtext =
+                "New standing with " .. TR_COLOR_NAME .. self.name .. TR_COLOR_RESUME .. " is " .. TR_COLOR_NAME .. newfaction .. TR_COLOR_RESUME .. "!"
+
+            Log(newstandingtext)
+        end
+
+        local remaining, nextstanding, plusminus
+        if diff > 0 then
+            remaining = barMax - barValue
+            if standingID < TR_REP_LEVEL_MAX then
+                nextstanding = GetColoredRepLevelName(0, standingID + 1)
+            else
+                nextstanding = "End of " .. _G["FACTION_STANDING_LABEL" .. TR_REP_LEVEL_MAX]
+            end
+        else
+            remaining = barValue - barMin
+            if standingID > TR_REP_LEVEL_MIN then
+                nextstanding = _G["FACTION_STANDING_LABEL" .. standingID - 1]
+            else
+                nextstanding = "Beginning of " .. _G["FACTION_STANDING_LABEL" .. TR_REP_LEVEL_MIN]
+            end
+        end
+
+        local change = math.abs(barValue - self.value)
+        local repetitions = math.ceil(remaining / change)
+
+        local togo_total = TR_EXALTED_AT - barValue
+        local reps_total = math.ceil(togo_total / math.abs(diff))
+
+        local newvaluetext = string.format(
+            "[ThorRep] %s %s, %s to %s (%s reps), %s to %s (%s reps)",
+            FormatColor(TR_COLOR_NR, "%+d", change),
+            FormatColor(TR_COLOR_NAME, self.name),
+            FormatColor(TR_COLOR_NR, "%d", remaining),
+            nextstanding,
+            FormatColor(TR_COLOR_NR, "%d", repetitions),
+            FormatColor(TR_COLOR_NR, "%d", togo_total),
+            GetColoredRepLevelName(0, TR_REP_LEVEL_MAX),
+            FormatColor(TR_COLOR_NR, "%d", reps_total)
+        )
+
+        Log(newvaluetext)
+
+        self.value = barValue
+        self.standingID = standingID
+    end
+end
+
+-- end
+
+-- Factions class
+Factions = {}
+Factions.__index = Factions
+
+function Factions:Create()
+    local object = {}
+    setmetatable(object, Factions)
+    object.factions_ = {}
+    object.factionIDs_ = {}
+    object.indexed_ = 0
+    return object
+end
+
+function Factions:Scan()
     LogDebug("Scanning factions...")
-    local nr_total = 0
-    local nr_inactive = 0
-    for i = 1, GetNumFactions() do
-        local name, _, standingID, _, _, barValue, _, _, isHeader, _, hasRep, _, _, factionID = GetFactionInfo(i)
-        if ((not isHeader or hasRep) and name) then
-            factionVars[name] = {}
-            factionVars[name].Standing = standingID
-            factionVars[name].Value = barValue
+    local to_index = GetNumFactions()
+    for index = 1, to_index do
+        local name, _, standingID, _, _, barValue, _, _, isHeader, _, hasRep, _, _, factionID = GetFactionInfo(index)
+        if (not isHeader or hasRep) then
 
-            nr_total = nr_total + 1
-            if IsFactionInactive(i) then
-                nr_inactive = nr_inactive + 1
-            end
-
-            local friendID, friendRep, friendMaxRep, friendName, friendText, friendTexture, friendTextLevel, friendThreshold, nextFriendThreshold = GetFriendshipReputation(factionID)
-            if friendID then
-                LogDebug("%s seems to be a friendship", name)
+            if self.factions_[factionID] == nil then
+                self.factions_[factionID] = Faction:Create(factionID)
+                self.factionIDs_[#self.factionIDs_ + 1] = factionID
             end
         end
     end
-    LogDebug("Found %d factions (%d inactive)", nr_total, nr_inactive)
+    self.indexed_ = to_index
+    LogDebug("Found %d factions", #self.factionIDs_)
 end
 
-local function Report() 
-    local tempfactions = GetNumFactions()
-    if (tempfactions ~= 0 and init == 0) then
-        ScanFactions()
-        init = 1
-        factions = tempfactions
-        return
-    end
-    if (tempfactions > factions) then
-        ScanFactions()
-        factions = tempfactions
-    end
-    for factionIndex = 1, GetNumFactions() do
-        local name, _, standingID, barMin, barMax, barValue, _, _, isHeader, _, hasRep = GetFactionInfo(factionIndex)
+function Factions:ShouldScan()
+    return self.indexed_ < GetNumFactions()
+end
 
-        if (not isHeader or hasRep) and factionVars[name] then
-            local diff = barValue - factionVars[name].Value
-            if diff ~= 0 then
-                if standingID ~= factionVars[name].Standing then
-                    local newfaction = _G["FACTION_STANDING_LABEL" .. standingID]
-                    local newstandingtext =
-                        "New standing with " .. TR_COLOR_NAME .. name .. TR_COLOR_RESUME .. " is " .. TR_COLOR_NAME .. newfaction .. TR_COLOR_RESUME .. "!"
-
-                    TR_CHATFRAME:AddMessage(newstandingtext)
-                end
-
-                local remaining, nextstanding, plusminus
-                if diff > 0 then
-                    remaining = barMax - barValue
-                    if standingID < TR_REP_LEVEL_MAX then
-                        nextstanding = GetColoredRepLevelName(0, standingID + 1)
-                    else
-                        nextstanding = "End of " .. _G["FACTION_STANDING_LABEL" .. TR_REP_LEVEL_MAX]
-                    end
-                else
-                    remaining = barValue - barMin
-                    if standingID > TR_REP_LEVEL_MIN then
-                        nextstanding = _G["FACTION_STANDING_LABEL" .. standingID - 1]
-                    else
-                        nextstanding = "Beginning of " .. _G["FACTION_STANDING_LABEL" .. TR_REP_LEVEL_MIN]
-                    end
-                   end
-
-                local change = math.abs(barValue - factionVars[name].Value)
-                local repetitions = math.ceil(remaining / change)
-
-                local togo_total = TR_EXALTED_AT - barValue
-                local reps_total = math.ceil(togo_total / math.abs(diff))
-
-                local newvaluetext = string.format(
-                    "[ThorRep] %s %s, %s to %s (%s reps), %s to %s (%s reps)",
-                    FormatColor(TR_COLOR_NR, "%+d", change),
-                    FormatColor(TR_COLOR_NAME, name),
-                    FormatColor(TR_COLOR_NR, "%d", remaining),
-                    nextstanding,
-                    FormatColor(TR_COLOR_NR, "%d", repetitions),
-                    FormatColor(TR_COLOR_NR, "%d", togo_total),
-                    GetColoredRepLevelName(0, TR_REP_LEVEL_MAX),
-                    FormatColor(TR_COLOR_NR, "%d", reps_total)
-                )
-
-                TR_CHATFRAME:AddMessage(newvaluetext)
-
-                factionVars[name].Value = barValue
-                factionVars[name].Standing = standingID
-            end
-        end
+function Factions:Update()
+    for id, faction in pairs(self.factions_) do
+        -- LogDebug("Updating %d...", id)
+        faction:Update()
     end
 end
 
-local function eventHandler(self, event, ...)
+-- end
+
+
+local factions = Factions:Create()
+
+local function HandleFactionUpdate()
+    if factions:ShouldScan() then factions:Scan() end
+
+    factions:Update()
+end
+
+local function HandleEvent(self, event, ...)
     if (event == "UPDATE_FACTION") then
-        Report()
+        HandleFactionUpdate()
     end
 end
 
 local frame = CreateFrame("FRAME", nil, UIParent)
 frame:RegisterEvent("UPDATE_FACTION")
-frame:SetScript("OnEvent", eventHandler)
+frame:SetScript("OnEvent", HandleEvent)
